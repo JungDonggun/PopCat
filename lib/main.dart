96e1564ff431c4a4312f4f74c6fd32018da8340d
JungDonggun <donggun.dev@gmail.com>
2021-03-06 18:34:29 +0900

    Add: image(PopCat on/off)

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,32 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(PopCatApp());
+void main() {
+  runApp(MaterialApp(
+    title: 'hi',
+    theme: ThemeData(
+      primarySwatch: Colors.amber,
+    ),
+    home: PopCatApp(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
 
 class PopCatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+            statusBarBrightness: Brightness.light) // Or Brightness.dark
+        );
+
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'PopCat',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
+        backgroundColor: Colors.black,
         body: Center(
-          child: Text('Hello World'),
+          child: Image.asset('assets/images/popcat_off.jpg'),
         ),
       ),
     );
